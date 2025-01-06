0f293935dc8d6a07b339654fbd82d4f4e0d11c3d
GOURAVSINGH19 <gouravsingh4495@example.com>
2025-01-06 18:39:06 +0530

    init 😶‍🌫️

--- actions/stream.actions.ts
+++ actions/stream.actions.ts
@@ -1,7 +1,7 @@
 'use server';
 
-import { currentUser } from "@clerk/nextjs/server";
-import { StreamClient } from "@stream-io/node-sdk";
+import { currentUser } from '@clerk/nextjs/server';
+import { StreamClient } from '@stream-io/node-sdk';
 
 const STREAM_API_KEY = process.env.NEXT_PUBLIC_STREAM_API_KEY;
 const STREAM_API_SECRET = process.env.STREAM_API_SECRET;
@@ -9,12 +9,12 @@ const STREAM_API_SECRET = process.env.STREAM_API_SECRET;
 export const tokenProvider = async () => {
   const user = await currentUser();
 
-  if (!user) throw new Error("User not Authenticated");
+  if (!user) throw new Error('User is not authenticated');
+  if (!STREAM_API_KEY) throw new Error('Stream API key secret is missing');
+  if (!STREAM_API_SECRET) throw new Error('Stream API secret is missing');
 
-  if (!STREAM_API_KEY || !STREAM_API_SECRET)
-    throw new Error("Missing Stream API Key");
+  const streamClient = new StreamClient(STREAM_API_KEY, STREAM_API_SECRET);
 
-  const streamClient = new StreamClient(STREAM_API_KEY,STREAM_API_SECRET);
   const expirationTime = Math.floor(Date.now() / 1000) + 3600;
   const issuedAt = Math.floor(Date.now() / 1000) - 60;
 
